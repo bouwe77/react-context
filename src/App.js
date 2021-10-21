319ea835bf3635ac55d7df3bc5c07b1ba095c989
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-10-21 12:01:39 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <CountProvider>
+      <div className="App">
+        <Header />
+        <DisplayCounter />
+
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <OtherStuff />
+          <CounterStuff />
+        </div>
+        <Footer />
+      </div>
+    </CountProvider>
+  );
+}
+
+function OtherStuff() {
+  React.useEffect(() => {
+    console.log("render OtherStuff");
+  });
+
+  return (
+    <div>
+      <h2>Other stuff</h2>
+      Hello World
+    </div>
+  );
+}
+
+function Header() {
+  React.useEffect(() => {
+    console.log("render Header");
+  });
+
+  return (
+    <header>
+      <h1>My App</h1>
+      <hr />
+      <br />
+    </header>
+  );
+}
+
+function Footer() {
+  React.useEffect(() => {
+    console.log("render Footer");
+  });
+
+  return (
+    <footer>
+      <hr />
+      Copyright &copy; Bouwe 2021
+    </footer>
+  );
+}
+
+function CounterStuff() {
+  React.useEffect(() => {
+    console.log("render CounterStuff");
+  });
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <UpdateCounter />
+    </div>
+  );
+}
+
+function DisplayCounter() {
+  React.useEffect(() => {
+    console.log("render DisplayCounter");
+  });
+
+  const {
+    state: { count }
+  } = useCount();
+
+  return <h1>{count}</h1>;
+}
+
+function UpdateCounter() {
+  const { dispatch } = useCount();
+
+  React.useEffect(() => {
+    console.log("render UpdateCounter");
+  });
+
+  return (
+    <>
+      <button
+        onClick={() => dispatch({ type: "decrement" })}
+        style={{ width: "100px", height: "100px", fontSize: "80px" }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => dispatch({ type: "increment" })}
+        style={{ width: "100px", height: "100px", fontSize: "80px" }}
+      >
+        +
+      </button>
+    </>
+  );
+}
+
+const CountContext = React.createContext();
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case "increment": {
+      return { count: state.count + 1 };
+    }
+    case "decrement": {
+      return { count: state.count - 1 };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+function CountProvider({ children }) {
+  const [state, dispatch] = React.useReducer(countReducer, { count: 0 });
+  // NOTE: you *might* need to memoize this value
+  // Learn more in http://kcd.im/optimize-context
+  const value = { state, dispatch };
+  return (
+    <CountContext.Provider value={value}>{children}</CountContext.Provider>
+  );
+}
+
+function useCount() {
+  const context = React.useContext(CountContext);
+  if (context === undefined) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+}
+
+export { CountProvider, useCount };
